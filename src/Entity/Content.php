6518550ba21c5e2c41aca3b18e06ace087892ac8
BrunoGaffo <67167417+BrunoGaffo@users.noreply.github.com>
2024-10-07 11:31:37 -0300

    up

--- src/Entity/Content.php
+++ src/Entity/Content.php
@@ -4,12 +4,16 @@ namespace ControleOnline\Entity;
 
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiFilter;
 use ControleOnline\Controller\DownloadOrderNFAction;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * Content
@@ -22,41 +26,44 @@ use Symfony\Component\Serializer\Annotation\Groups;
     routePrefix: '/ead', // Adiciona o prefixo para as rotas dessa entidade
     operations: [
         new Get(
-            security: 'is_granted(\'ROLE_CLIENT\')'
-        ),
-
-        new Post(
-            
-            deserialize: false,
             security: 'is_granted(\'ROLE_CLIENT\')',
-            validationContext: ['groups' => ['Default', 'order_upload_nf']],
-            openapiContext: ['consumes' => ['multipart/form-data']]
-        )
+        ),
+        new GetCollection(
+            security: 'is_granted(\'ROLE_CLIENT\')',
+        ),
+        new Post(
+            security: 'is_granted(\'ROLE_CLIENT\')',
+        ),
+        new Put(
+            security: 'is_granted(\'ROLE_CLIENT\')',
+        ),
+        new Delete(
+            security: 'is_granted(\'ROLE_CLIENT\')',
+        ),
     ],
-    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
-    normalizationContext: ['groups' => ['invoice_tax_read']],
-    denormalizationContext: ['groups' => ['invoice_tax_write']]
-)] 
+    normalizationContext: ['groups' => ['content_read']],
+    denormalizationContext: ['groups' => ['content_write']]
+)]
 class Content
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     **/
+
+    #[ApiFilter(filterClass: SearchFilter::class, properties: ['id' => 'exact'])]
     private $id;
 
     /**
-     * @var string
+     *
      *
      * @ORM\Column(name="content", type="string", length=255, nullable=false)
      */
     private $content;
 
     /**
-     * @var \Category
+     * 
      *
      * @ORM\ManyToOne(targetEntity="Category")
      * @ORM\JoinColumns({
@@ -66,7 +73,7 @@ class Content
     private $subjects;
 
     /**
-     * @var \File
+     *
      *
      * @ORM\ManyToOne(targetEntity="File")
      * @ORM\JoinColumns({
@@ -74,4 +81,76 @@ class Content
      * })
      */
     private $file;
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     */
+    public function setContent($content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of subjects
+     */
+    public function getSubjects()
+    {
+        return $this->subjects;
+    }
+
+    /**
+     * Set the value of subjects
+     */
+    public function setSubjects($subjects): self
+    {
+        $this->subjects = $subjects;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of file
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set the value of file
+     */
+    public function setFile($file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
 }
